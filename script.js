852a9d9aa6041f2dc2f4f82a133ae8aafa5a7f24
이승태 <teemo@mongle.io>
2020-10-16 15:46:55 +0900

    이슈 2번 완료

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const monsterStone = document.querySelector(".monster-stone"),
-    monsterButtons = document.querySelectorAll(".create-monster .monster-button"),
+    monsterButtons = document.querySelectorAll(
+        ".create-monster .monster-button"
+    ),
     SHOW_CLASS = "is-show",
     HIDE_CLASS = "is-hide";
 
-console.log("monsterStone")
-console.log(monsterStone)
-console.log("monsterButtons")
-console.log(monsterButtons)
+console.log("monsterStone");
+console.log(monsterStone);
+console.log("monsterButtons");
+console.log(monsterButtons);
 function genMonster() {
-    console.log("genMonster")
+    console.log("genMonster");
     monsterStone.classList.add(SHOW_CLASS);
 }
 
-
 function init() {
-    console.log("init")
+    console.log("init");
 
     if (monsterButtons) {
         Array.from(monsterButtons).forEach(function (currentMonsterButton) {
