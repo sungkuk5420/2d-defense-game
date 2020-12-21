4bc70f358be8e76d878996b553f4ed971fa05dc6
seungtae97 <fo_ma@naver.com>
2020-12-21 16:13:41 +0900

    몬스터 이동, 타워 접촉체크, 몬스터 삭제

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const monsterStone = document.querySelector(".monster-stone"),
     SHOW_CLASS = "is-show",
     HIDE_CLASS = "is-hide";
 
+let stopFlag = false;
+
 console.log("monsterStone");
 console.log(monsterStone);
 console.log("monsterButtons");
@@ -15,11 +17,40 @@ function towerHp(params) {}
 function genMonster() {
     console.log("genMonster");
     monsterStone.classList.add(SHOW_CLASS);
-    gsap.to(monsterStone, {
-        x: -200,
-        duration: 1.2,
+    moveMonster(monsterStone);
+}
+
+function checkConnectTower() {
+    if (monsterStone.style.transform == "") {
+        return false;
+    }
+    const xPosition = monsterStone.style.transform.match(/(-?[0-9\.]+)/g)[0];
+    const yPosition = monsterStone.style.transform.match(/(-?[0-9\.]+)/g)[1];
+    console.log(parseInt(xPosition));
+    if (parseInt(xPosition) < -780) {
+        stopFlag = true;
+    }
+}
+
+function moveMonster(currentMonster) {
+    setTimeout(() => {
+        gsap.to(currentMonster, {
+            x: "-=10",
+            duration: 0.1,
+        });
+        checkConnectTower();
+        if (!stopFlag) {
+            moveMonster(currentMonster);
+        } else {
+            hideMonstaer(currentMonster);
+        }
+    }, 100);
+}
+
+function hideMonstaer(currentMonster) {
+    gsap.to(currentMonster, {
+        duration: 1,
         opacity: 0,
-        rotate: -15,
     });
 }
 
